9d4d57bb928bd2496cead0b6ea5f3af1cce659a4
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-06-16 09:28:13 -0300

    Add files via upload

---
+++ Aula02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+//colocar no git e mandar o link no discord PRESENCA
+//'text dfdf' //Um parametro so
+int main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "Uso: " << argv[0] << " add <nome>";
+		return 0;
+	}
+	else if (std::strcmp(argv[1],"add") == 0 && argc == 2)
+	{
+		std::cout << "Digite a mensagem: ";
+		std::string code;
+		std::getline(std::cin, code);
+
+		std::cout << "mensagem imprimida" << std::endl;
+		return 0;
+	}
+	else if (std::strcmp(argv[1],"add") && argc > 2)
+	{
+		std::cout << "Mensagem: " << argv[3] << " adicionada" << std::endl;
+		return 0;
+	}
+	
+	return 1;
+}
