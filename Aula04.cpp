d75d61abaa6a094f533b64c73bc3968d89b6c565
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-06-18 10:41:16 -0300

    Add files via upload

---
+++ Aula04.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+int list()
+{
+	std::ifstream file;
+	file.open("arquivo.txt");
+
+
+	if (!file.is_open())
+	{
+		std::cout << "Erro: o arquivo nao esta aberto" << std::endl;
+		return 1;
+	}
+
+	std::string message;
+	int line_number{0};
+
+	while(!file.eof())
+	{
+		getline(file, message);
+		if (message.size() == 0)
+		{
+			continue;
+		}
+
+		std::cout << line_number << ". " << message << std::endl;
+	}
+
+	return 0;
+}
+
+
+bool write_file(const std::string& mensagem)
+{
+	std::ofstream file;
+	file.open("arquivo.txt", std::ios_base::app);
+
+	if (file.fail())
+	{
+		std::cout << "Erro ao abrir o arquivo" << std::endl;
+		return false;
+	}
+	if (file.is_open())
+	{
+		file << mensagem << std::endl;
+		std::cout << "Mensagem salva no arquivo" << std::endl;
+		file.close();
+		return true;
+	}
+	
+	std::cout << "Falha ao escrever no arquivo" << std::endl;
+	return false;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "Uso: " << argv[0] << " add <nome>";
+		return 0;
+	}
+
+	std::string parameter1 = argv[1];
+	if (parameter1 == "add" && argc == 2)
+	{
+		std::cout << "Digite a mensagem: ";
+		std::string code;
+		std::getline(std::cin, code);
+
+		std::cout << "mensagem imprimida" << std::endl;
+		return 0;
+	}
+
+	std::string mensagem = argv[2];
+	if (parameter1 == "add" && argc > 2)
+	{
+		std::cout << "Mensagem: " << mensagem << " adicionada" << std::endl;
+		write_file(mensagem);
+		return 0;
+	}
+
+	if (parameter1 == "list" && argc == 2)
+	{
+		return list();
+	}
+
+
+	return 1;
+}
