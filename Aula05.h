3e5212b7726a3333f129872e49ba5dd788d768d0
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-06-22 09:58:33 -0300

    Add files via upload

---
+++ Aula05.h
@@ -0,0 +1,50 @@
+#include <string>
+
+struct Time
+{
+	std::string hora;
+
+	std::string getHora();
+	void setHora(const std::string& _hora);
+
+	
+};
+
+struct Date
+{
+	std::string data;
+
+	std::string getData();
+	void setData(const std::string& _data);
+
+	void increaseDay();
+	void increaseMonth();
+	void increaseYear();
+};
+
+struct Content
+{
+	std::string mensagem;
+
+	std::string getMensagem();
+	void setMensagem(const std::string& _mensagem);
+
+	void UpperCase();
+	void LowerCase();
+
+	void concatenate(const std::string& other_string);
+	void multiplicate(const int numberTimes);
+};
+
+struct Message
+{
+	Time time;
+	Date date;
+	Content content;
+
+	bool compare_dates(const Message& other_dates);
+	bool compare_times(const Message& other_times);
+
+	void WriteFile();
+	void print();
+};
