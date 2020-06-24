9864fd9f91d16408d4d8a3fd973e757771748d4e
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-06-24 10:10:14 -0300

    Update Diary.cpp

--- Aula07/src/Diary.cpp
+++ Aula07/src/Diary.cpp
@@ -1,1 +1,45 @@
 
+#include "Diary.h"
+
+
+Diary::Diary(const std::string& filename)
+	: filename{filename}, messages{nullptr},messages_size{0}, messages_capacity{100}
+
+  {
+    messages = new Message[10];
+    messages_size = 0;
+  }
+
+void Diary::add(const std::string& message)
+{
+    // adicionar mensagem no array de mensagens
+	Message example
+  example.content = message;
+
+    if (messages_size < messages_capacity)
+    {
+    	messages[messages_size] = example;
+    	messages_size++;
+    }
+    else{
+
+      Message *buff = new Message[messages_size + 1];
+      messages_capacity++;
+      messages_size++;
+
+      buff = messages;
+
+      delete[] messages;
+
+      messages = buff;
+      messages[messages_size] = example;
+
+    }
+
+}
+
+int Diary::write()
+{
+    // gravar as mensagens no disco
+}
+
