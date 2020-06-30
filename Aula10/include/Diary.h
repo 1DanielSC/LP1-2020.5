6e3376cf7d18ecd7102e461ccab7475f9b51c712
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-06-30 10:39:28 -0300

    Create Diary.h

---
+++ Aula10/include/Diary.h
@@ -0,0 +1,25 @@
+#ifndef W5N_DIARY_H
+#define W5N_DIARY_H
+
+#include "Message.h"
+
+#include <string>
+
+struct Diary
+{
+    Diary(const std::string& filename);
+    ~Diary();
+
+    std::string filename;
+    Message* messages;
+    size_t messages_size;
+    size_t messages_capacity;
+
+    void add(const std::string& message);
+    bool write();
+    bool load();
+    bool verifyDate(const std::string& filename, const std::string& date);
+    Message* IncreaseSize();
+    Message* search(const std::string& s);
+};
+#endif
