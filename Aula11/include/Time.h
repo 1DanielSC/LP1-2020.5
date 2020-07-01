0b58b79ad39e266d728af11de2b3d61ee6077420
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-07-01 12:39:29 -0300

    Add files via upload

---
+++ Aula11/include/Time.h
@@ -0,0 +1,16 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <string>
+
+struct Time
+{
+	Time();
+    unsigned hour;
+    unsigned minute;
+    unsigned second;
+
+    void set_from_string(const std::string& time);
+    std::string to_string();
+};
+#endif
