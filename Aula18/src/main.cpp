fe2fb5e89698599c07b65c69d4a9cbf2748de5b3
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-07-20 15:52:43 -0300

    Update main.cpp

--- Aula18/src/main.cpp
+++ Aula18/src/main.cpp
@@ -14,17 +14,17 @@ int main(int argc, char const *argv[])
 	ClientePF exemplo{"Daniel", "10/08/2018" ,"108.999.999-48", 20, "98825-1260"};
 	exemplo.display();
 
-  ClientePJ empresa{"Extra", "10/08/2000", "64.808.316/0001-97", "Extra ltda"};
-  empresa.display();
+  	ClientePJ empresa{"Extra", "10/08/2000", "64.808.316/0001-97", "Extra ltda"};
+  	empresa.display();
 	
-  Gerente exemplo1{"Daniel", 18, "Gerente", 50};
-  exemplo1.display();
+  	Gerente exemplo1{"Daniel", 18, "Gerente", 50};
+  	exemplo1.display();
 	
-  Caixa exemplo2{"Daniel", 18, 50, 50, 520.99};
-  exemplo2.display();
+  	Caixa exemplo2{"Daniel", 18, 50, 50, 520.99};
+  	exemplo2.display();
 
-  Seguranca exemplo3("Daniel", 18, "Frutas", "Integral");
-  exemplo3.display();
+  	Seguranca exemplo3("Daniel", 18, "Frutas", "Integral");
+  	exemplo3.display();
 
 	return 0;
 }
