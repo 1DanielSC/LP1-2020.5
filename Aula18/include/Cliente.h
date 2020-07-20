b3d4459f8da1967483bbf80ed2b009522ef00a40
1DanielSC <66954777+1DanielSC@users.noreply.github.com>
2020-07-20 16:34:44 -0300

    Create Cliente.h

---
+++ Aula18/include/Cliente.h
@@ -0,0 +1,23 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include <string>
+
+class Cliente
+{
+private:
+	std::string Nome;
+	std::string DataAdmissao;
+
+public:
+
+	Cliente(std::string nome = "None", std::string dataAdmissao = "xx/xx/xxxx");
+
+	void setNome(std::string nome);
+	void setDataAdmissao(std::string dataAdmissao);
+
+  	std::string getNome();
+  	std::string getDataAdmissao();	
+};
+
+#endif
